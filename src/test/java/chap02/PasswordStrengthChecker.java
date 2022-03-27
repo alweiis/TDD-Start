a30dafedd8ade44cbdf5653187f40ad1f1283c15
alweiis <jstm1207@gmail.com>
2022-03-27 18:35:44 +0900

    feat: 암호 검사기 테스트 코드 작성

--- src/test/java/chap02/PasswordStrengthChecker.java
+++ src/test/java/chap02/PasswordStrengthChecker.java
@@ -3,12 +3,33 @@ package chap02;
 public class PasswordStrengthChecker {
 
     public PasswordStrength checker(String s) {
-        if (s.length() < 8) {
+        if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
+        boolean lengthEnough = s.length() >= 8;
+        boolean containsNum = CheckContainingNumberCriteria(s);
+        boolean containsUpp = CheckContainingUppercaseCriteria(s);
+
+        if (lengthEnough && !containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
+        if (!lengthEnough && containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
+        if (!lengthEnough && !containsNum && containsUpp)
+            return PasswordStrength.WEAK;
+
+        if (!lengthEnough) {
             return PasswordStrength.NORMAL;
         }
-        boolean containsNum = CheckContainingNumberCriteria(s);
         if (!containsNum) return PasswordStrength.NORMAL;
+        if (!containsUpp) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
+    }
+
+    private boolean CheckContainingUppercaseCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean CheckContainingNumberCriteria(String s) {
--- src/test/java/chap02/PasswordStrengthCheckerTest.java
+++ src/test/java/chap02/PasswordStrengthCheckerTest.java
@@ -15,7 +15,7 @@ public class PasswordStrengthCheckerTest {
 
     @Test
     void 모든_규칙을_통과하는_경우() {
-        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc123AB", PasswordStrength.STRONG);
         assertStrength("ab12!@AB", PasswordStrength.STRONG);
     }
 
@@ -27,6 +27,36 @@ public class PasswordStrengthCheckerTest {
 
     @Test
     void 숫자를_포함하지_않고_나머지_조건은_충족하는_경우() {
-        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+        assertStrength("ABCDabcd!@", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void 입력값이_없는_경우() {
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    @Test
+    void 입력값이_빈_문자열인_경우() {
+        assertStrength("", PasswordStrength.INVALID);
+    }
+
+    @Test
+    void 대문자를_포함하지_않고_나머지_조건을_충족하는_경우() {
+        assertStrength("abcd1234", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void 길이가_8글자_이상인_조건만_충족하는_경우() {
+        assertStrength("abcdefgh", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void 숫자_포함_조건만_충족하는_경우() {
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void 대문자_포함_조간만_충족하는_경우() {
+        assertStrength("ABCDE", PasswordStrength.WEAK);
     }
 }
